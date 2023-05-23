8e4bfbadce1d7d7d2c389301709c4b1081b6f5ba
Eibon <ninetimes64@gmail.com>
2023-05-23 16:23:21 -0500

    touched up conflicts

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -3,8 +3,37 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+// Add and configure workbox plugins for a service worker and manifest file
+const serviceWorkerConfig = {
+  swSrc: './src/js/sw.js',
+  swDest: 'sw.js',
+  exclude: [/\.map$/, /manifest.*\.js(?:on)?$/, /\.html$/],
+  importsDirectory: 'wb-assets',
+};
+
+const manifestConfig = {
+  filename: 'manifest.json',
+  name: 'PWA Budget Tracker',
+  short_name: 'Budget Tracker',
+  description: 'A simple budget tracker PWA',
+  background_color: '#ffffff',
+  theme_color: '#ffffff',
+  'theme-color': '#ffffff',
+  start_url: '/',
+  icons: [
+    {
+      src: path.resolve('src/images/icons/icon-512x512.png'),
+      sizes: [72, 96, 128, 144, 152, 192, 384, 512],
+      destination: path.join('assets', 'icons'),
+    },
+  ],
+};
+
+
+// Add CSS loaders and babel to webpack.
+
+
+
 
 module.exports = () => {
   return {
